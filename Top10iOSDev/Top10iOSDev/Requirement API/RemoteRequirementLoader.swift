1098e8fff30a6bf907e0f999ce842eb2e5cc8d5c
Romain <rbrunie@cityway.fr>
2021-01-15 09:05:17 +0100

    replace magic number with variable giving extra context

--- Top10iOSDev/Top10iOSDev/Requirement API/RemoteRequirementLoader.swift
+++ Top10iOSDev/Top10iOSDev/Requirement API/RemoteRequirementLoader.swift
@@ -97,9 +97,11 @@ private class RequirementCategoryMapper {
             }
         }
     }
+    
+    static var OK_200: Int { return 200 }
 
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [RequirementCategory] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteRequirementLoader.Error.invalidData
         }
         
