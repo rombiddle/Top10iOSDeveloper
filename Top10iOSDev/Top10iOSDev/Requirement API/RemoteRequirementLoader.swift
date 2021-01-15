b5024ed20216240ef28909ddb761f2ba8e6e5bb8
Romain <rbrunie@cityway.fr>
2021-01-15 08:59:45 +0100

    Moce mapping logic with a new type 'RequirementCategoryMapper'

--- Top10iOSDev/Top10iOSDev/Requirement API/RemoteRequirementLoader.swift
+++ Top10iOSDev/Top10iOSDev/Requirement API/RemoteRequirementLoader.swift
@@ -39,11 +39,10 @@ public final class RemoteRequirementLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data) {
-                    if let root = try? JSONDecoder().decode(Root.self, from: data) {
-                        completion(.success(root.categories.map { $0.category }))
-                    }
-                } else {
+                do {
+                    let categories = try RequirementCategoryMapper.map(data, response)
+                    completion(.success(categories))
+                } catch {
                     completion(.failure(.invalidData))
                 }
         
@@ -54,46 +53,57 @@ public final class RemoteRequirementLoader {
     }
 }
 
-private struct Root: Decodable {
-    let categories: [Category]
-}
-
-private struct Category: Decodable {
-    let id: UUID
-    let name: String
-    let groups: [Group]
-    
-    var category: RequirementCategory {
-        RequirementCategory(id: id,
-                            name: name,
-                            groups: groups.map { $0.group })
+private class RequirementCategoryMapper {
+    private struct Root: Decodable {
+        let categories: [Category]
     }
-}
 
-private struct Group: Decodable {
-    let id: UUID
-    let name: String
-    let items: [Item]
-    
-    var group: RequirementGroup {
-        RequirementGroup(id: id,
-                         name: name,
-                         items: items.compactMap { $0.item })
-    }
-}
-
-private struct Item: Decodable {
-    let id: UUID
-    let name: String
-    let type: Int
-    
-    var item: RequirementItem? {
-        if let rType = RequirementType(type: type) {
-            return RequirementItem(id: id,
-                                   name: name,
-                                   type: rType)
-        } else {
-            return nil
+    private struct Category: Decodable {
+        let id: UUID
+        let name: String
+        let groups: [Group]
+        
+        var category: RequirementCategory {
+            RequirementCategory(id: id,
+                                name: name,
+                                groups: groups.map { $0.group })
         }
+    }
+
+    private struct Group: Decodable {
+        let id: UUID
+        let name: String
+        let items: [Item]
+        
+        var group: RequirementGroup {
+            RequirementGroup(id: id,
+                             name: name,
+                             items: items.compactMap { $0.item })
+        }
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let name: String
+        let type: Int
+        
+        var item: RequirementItem? {
+            if let rType = RequirementType(type: type) {
+                return RequirementItem(id: id,
+                                       name: name,
+                                       type: rType)
+            } else {
+                return nil
+            }
+        }
+    }
+
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [RequirementCategory] {
+        guard response.statusCode == 200 else {
+            throw RemoteRequirementLoader.Error.invalidData
+        }
+        
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.categories.map { $0.category }
     }
 }
