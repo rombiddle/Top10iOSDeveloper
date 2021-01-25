283fc1317f8af26becbbd080e2f45c805cfa5d0d
Romain <rbrunie@cityway.fr>
2021-01-25 08:59:12 +0100

    Save command requests cache deletion

--- Top10iOSDev/Top10iOSDevTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ Top10iOSDev/Top10iOSDevTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Top10iOSDev
 
 class LocalRequirementLoader {
     let store: RequirementStore
@@ -13,10 +14,18 @@ class LocalRequirementLoader {
     init(store: RequirementStore) {
         self.store = store
     }
+    
+    func save(_ items: [RequirementCategory]) {
+        store.deleteCachedRequirements()
+    }
 }
 
 class RequirementStore {
     var deleteCachedRequirementsCallCount: Int = 0
+    
+    func deleteCachedRequirements() {
+        deleteCachedRequirementsCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -26,6 +35,22 @@ class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalRequirementLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedRequirementsCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = RequirementStore()
+        let sut = LocalRequirementLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedRequirementsCallCount, 1)
+    }
+    
+    // MARK: Helpers
+    
+    private func uniqueItem() -> RequirementCategory {
+        RequirementCategory(id: UUID(), name: "any", groups: [])
     }
 
 }
