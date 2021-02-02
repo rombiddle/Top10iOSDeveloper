28e77861157467323ee4a30138bc46f91ddfce72
Romain <rbrunie@cityway.fr>
2021-02-02 08:47:44 +0100

    Guarantee that the `LocalRequirementLoader` does not deliver insertion error after instance has been deallocated

--- Top10iOSDev/Top10iOSDevTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ Top10iOSDev/Top10iOSDevTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -20,7 +20,10 @@ class LocalRequirementLoader {
             guard let self = self else { return }
             
             if error == nil {
-                self.store.insert(items, completion: completion)
+                self.store.insert(items) { [weak self] error in
+                    guard self != nil else { return }
+                    completion(error)
+                }
             } else {
                 completion(error)
             }
@@ -111,7 +114,21 @@ class CacheFeedUseCaseTests: XCTestCase {
         
         sut = nil
         store.completeDeletion(with: anyNSError())
+
+        XCTAssertTrue(receivedResult.isEmpty)
+    }
+    
+    func test_save_doesNotDeliverInsertionErrorAfterSUTInstanceHasBeenDeallocated() {
+        let store = RequirementStoreSpy()
+        var sut: LocalRequirementLoader? = LocalRequirementLoader(store: store)
         
+        var receivedResult = [Error?]()
+        sut?.save([uniqueItem()], completion: { receivedResult.append($0) })
+        
+        store.completeDeletionSuccessfully()
+        sut = nil
+        store.completeInsertion(with: anyNSError())
+
         XCTAssertTrue(receivedResult.isEmpty)
     }
     
