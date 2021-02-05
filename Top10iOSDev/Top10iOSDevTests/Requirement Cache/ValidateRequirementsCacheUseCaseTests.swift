399698f3c6c8698ad90b242ffa01e67d60c16056
Romain <rbrunie@cityway.fr>
2021-02-05 17:18:34 +0100

    LocalRequirementLoader does not message store upon creation (before validating the cached requirements)

---
+++ Top10iOSDev/Top10iOSDevTests/Requirement Cache/ValidateRequirementsCacheUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  ValidateRequirementsCacheUseCaseTests.swift
+//  Top10iOSDevTests
+//
+//  Created by Romain Brunie on 05/02/2021.
+//
+
+import XCTest
+import Top10iOSDev
+
+class ValidateRequirementsCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalRequirementLoader, store: RequirementStoreSpy) {
+        let store = RequirementStoreSpy()
+        let sut = LocalRequirementLoader(store: store)
+        trackForMemotyLeaks(sut, file: file, line: line)
+        trackForMemotyLeaks(store, file: file, line: line)
+        return (sut, store)
+    }
+
+}
