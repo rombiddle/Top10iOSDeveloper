8cba1bc6077f47b1047198d4dd804dd7706d2e86
Romain <rbrunie@cityway.fr>
2021-02-24 08:45:43 +0100

    Extract `RequirementStoreSpecs` checks into reusable assertions

--- Top10iOSDev/Top10iOSDevTests/Requirement Cache/CodableRequirementStoreTests.swift
+++ Top10iOSDev/Top10iOSDevTests/Requirement Cache/CodableRequirementStoreTests.swift
@@ -25,31 +25,25 @@ class CodableRequirementStoreTests: XCTestCase, FailableRequirementStore {
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
         
-        expect(sut, toRetrieve: .empty)
+        assertThatRetrieveEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
         
-        expect(sut, toRetrieveTwice: .empty)
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
         let sut = makeSUT()
-        let requirements = uniqueItems().locals
         
-        insert(requirements, to: sut)
-        
-        expect(sut, toRetrieve: .found(requirements: requirements))
+        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
         let sut = makeSUT()
-        let requirements = uniqueItems().locals
         
-        insert(requirements, to: sut)
-        
-        expect(sut, toRetrieveTwice: .found(requirements: requirements))
+        assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFailureOnRetrievalError() {
@@ -73,29 +67,19 @@ class CodableRequirementStoreTests: XCTestCase, FailableRequirementStore {
     func test_insert_deliversNoErrorOnEmptyCache() {
         let sut = makeSUT()
         
-        let insertionError = insert(uniqueItems().locals, to: sut)
-        
-        XCTAssertNil(insertionError, "Expected to override cache successfully")
+        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
     }
     
     func test_insert_deliversNoErrorOnNonEmptyCache() {
         let sut = makeSUT()
-        insert(uniqueItems().locals, to: sut)
         
-        let lastestRequirements = uniqueItems().locals
-        insert(lastestRequirements, to: sut)
-        
-        expect(sut, toRetrieve: .found(requirements: lastestRequirements))
+        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
     func test_insert_overridesPreviouslyInsertedCacheValues() {
         let sut = makeSUT()
-        insert(uniqueItems().locals, to: sut)
         
-        let lastestRequirements = uniqueItems().locals
-        insert(lastestRequirements, to: sut)
-        
-        expect(sut, toRetrieve: .found(requirements: lastestRequirements))
+        assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
     
     func test_insert_deliversErrorOnInsertionError() {
@@ -121,35 +105,25 @@ class CodableRequirementStoreTests: XCTestCase, FailableRequirementStore {
     func test_delete_deliversNoErrorOnEmptyCache() {
         let sut = makeSUT()
         
-        let deletionError = deleteCache(from: sut)
-
-        XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
+        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
         
-        deleteCache(from: sut)
-
-        expect(sut, toRetrieve: .empty)
+        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_delete_deliversNoErrorOnNonEmptyCache() {
         let sut = makeSUT()
-        insert(uniqueItems().locals, to: sut)
-
-        let deletionError = deleteCache(from: sut)
-
-        XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
+        
+        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
     func test_delete_emptiesPreviouslyInsertedCache() {
         let sut = makeSUT()
-        insert(uniqueItems().locals, to: sut)
-
-        deleteCache(from: sut)
-
-        expect(sut, toRetrieve: .empty)
+        
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
     }
     
     func test_delete_deliversErrorOnDeletionError() {
@@ -172,28 +146,8 @@ class CodableRequirementStoreTests: XCTestCase, FailableRequirementStore {
     
     func test_storeSideEffects_runSerially() {
         let sut = makeSUT()
-        var completedOperationsInOrder = [XCTestExpectation]()
         
-        let op1 = expectation(description: "Operation 1")
-        sut.insert(uniqueItems().locals) { _ in
-            completedOperationsInOrder.append(op1)
-            op1.fulfill()
-        }
-        
-        let op2 = expectation(description: "Operation 2")
-        sut.deleteCachedRequirements { _ in
-            completedOperationsInOrder.append(op2)
-            op2.fulfill()
-        }
-        
-        let op3 = expectation(description: "Operation 3")
-        sut.insert(uniqueItems().locals) { _ in
-            completedOperationsInOrder.append(op3)
-            op3.fulfill()
-        }
-        
-        waitForExpectations(timeout: 5.0)
-        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
+        assertThatSideEffectsRunSerially(on: sut)
     }
     
     // - MARK: Helpers
--- Top10iOSDev/Top10iOSDevTests/Requirement Cache/XCTestCase+RequirementStoreSpecs.swift
+++ Top10iOSDev/Top10iOSDevTests/Requirement Cache/XCTestCase+RequirementStoreSpecs.swift
@@ -9,6 +9,116 @@ import XCTest
 import Top10iOSDev
 
 extension RequirementStoreSpecs where Self: XCTestCase {
+
+    func assertThatRetrieveDeliversEmptyOnEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+
+    func assertThatRetrieveHasNoSideEffectsOnEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        expect(sut, toRetrieveTwice: .empty, file: file, line: line)
+    }
+
+    func assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        let requirements = uniqueItems().locals
+                
+        insert(requirements, to: sut)
+                
+        expect(sut, toRetrieve: .found(requirements: requirements))
+    }
+
+    func assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        let requirements = uniqueItems().locals
+                
+        insert(requirements, to: sut)
+                
+        expect(sut, toRetrieveTwice: .found(requirements: requirements))
+    }
+
+    func assertThatInsertDeliversNoErrorOnEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        let insertionError = insert(uniqueItems().locals, to: sut)
+                
+        XCTAssertNil(insertionError, "Expected to override cache successfully")
+    }
+
+    func assertThatInsertDeliversNoErrorOnNonEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        insert(uniqueItems().locals, to: sut)
+                
+        let lastestRequirements = uniqueItems().locals
+        insert(lastestRequirements, to: sut)
+                
+        expect(sut, toRetrieve: .found(requirements: lastestRequirements))
+    }
+
+    func assertThatInsertOverridesPreviouslyInsertedCacheValues(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        insert(uniqueItems().locals, to: sut)
+                
+        let lastestRequirements = uniqueItems().locals
+        insert(lastestRequirements, to: sut)
+                
+        expect(sut, toRetrieve: .found(requirements: lastestRequirements))
+    }
+
+    func assertThatDeleteDeliversNoErrorOnEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        let deletionError = deleteCache(from: sut)
+
+        XCTAssertNil(deletionError, "Expected empty cache deletion to succeed", file: file, line: line)
+    }
+
+    func assertThatDeleteHasNoSideEffectsOnEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        deleteCache(from: sut)
+
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+
+    func assertThatDeleteDeliversNoErrorOnNonEmptyCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        insert(uniqueItems().locals, to: sut)
+
+        let deletionError = deleteCache(from: sut)
+
+        XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
+
+    }
+
+    func assertThatDeleteEmptiesPreviouslyInsertedCache(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        insert(uniqueItems().locals, to: sut)
+
+        deleteCache(from: sut)
+
+        expect(sut, toRetrieve: .empty)
+    }
+
+    func assertThatSideEffectsRunSerially(on sut: RequirementStore, file: StaticString = #file, line: UInt = #line) {
+        var completedOperationsInOrder = [XCTestExpectation]()
+                
+        let op1 = expectation(description: "Operation 1")
+        sut.insert(uniqueItems().locals) { _ in
+            completedOperationsInOrder.append(op1)
+            op1.fulfill()
+        }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.deleteCachedRequirements { _ in
+            completedOperationsInOrder.append(op2)
+            op2.fulfill()
+        }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(uniqueItems().locals) { _ in
+            completedOperationsInOrder.append(op3)
+            op3.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5.0)
+        XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
+     }
+
+ }
+
+extension RequirementStoreSpecs where Self: XCTestCase {
+    func assertThatRetrieveEmptyOnEmptyCache(on sut: RequirementStore, file: StaticString = #filePath, line: UInt = #line) {
+        expect(sut, toRetrieve: .empty)
+    }
+    
     @discardableResult
     func insert(_ requirements: [LocalRequirementCategory], to sut: RequirementStore) -> Error? {
         let exp = expectation(description: "Wait for cache insertion")
