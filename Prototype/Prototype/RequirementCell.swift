c971d0a8e06f79f08c0ab0f6963704a0b73b2082
Romain <rbrunie@cityway.fr>
2021-03-22 08:54:56 +0100

    Add `RequirementCell` with storyboard connections

---
+++ Prototype/Prototype/RequirementCell.swift
@@ -0,0 +1,13 @@
+//
+//  RequirementCell.swift
+//  Prototype
+//
+//  Created by Romain Brunie on 22/03/2021.
+//
+
+import UIKit
+
+final class RequirementCell: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var completedItemsDescription: UILabel!
+}
